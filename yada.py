d77e7c70958d33b2ea518b10fa0803b2cd09fcbd
dyada <ygiri32@gmail.com>
2023-03-24 19:19:48 +0530

    this is yada added code

--- yada.py
+++ yada.py
@@ -2,3 +2,4 @@ print("hello yadagiri ")
 print(" today is sunday full of rain in bangalore")
 
 print (" hello welcome to github.com")
+print (" **********************hello welcome to github.com+++++++++++++++++++++")
